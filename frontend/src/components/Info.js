1c77d23e1ac34476653b217134f08da89c29d520
Jesse Ayegba <jessejoseph64@gmail.com>
2021-01-18 13:00:00 +0100

    New components added to landing page

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Info.css";
+
+function Info() {
+  return (
+    <div className="info">
+      <div className="info__wrapper">
+        <h1 className="info__header">What is Ejeka Invest?</h1>
+        <p className="info__body">
+          The word "Ejeka" means "Let us". Thus, Ejeka Invest is a solution that
+          was built to allow us make and manage more secure investments. Our
+          platform allows you keep track of your ROI (Return on Investment) in
+          real time as well as make enquiries concerning its growth. Yes! You heard it - real time.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
---
+++ frontend/src/data/hero.js
@@ -0,0 +1,23 @@
+import investImage from "../svgs/invest.svg";
+import mobile from "../svgs/mobile.svg";
+import guaranteeImage from "../svgs/guarantee.svg";
+
+export const invest = {
+  header: <h1 className="heroSection__header">Ejeka Invest</h1>,
+  body:
+    "A platform that empowers you like never before, to make investments and get returns from those investments",
+  button: <button className="heroSection__btn">Create an Account</button>,
+  image: investImage,
+};
+export const deposit = {
+  header: <h1 className="heroSection__header">Easy Deposit</h1>,
+  body: "Make one click deposits that are super fast and secure",
+  image: mobile,
+  flex: "row-reverse",
+};
+export const guarantee = {
+  header: <h1 className="heroSection__header">Guaranteed Returns (ROI)</h1>,
+  body: "We provide guaranteed returns on investments. It's in our DNA",
+  image: guaranteeImage,
+  imageWidth: "400px",
+};
