99fd7c5c192bb0395b8d03ecca92985c3b90c6a9
Jesse Ayegba <jessejoseph64@gmail.com>
2021-01-22 13:26:22 +0100

    More style added to signup page

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUpForm.css";
+import { Link } from "react-router-dom";
 
 function SignUpForm() {
+  const [ userData, setUserData ] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  })
+  const handleChange = (e) => {
+
+  }
   return (
     <div className="signUpForm">
       <form className="signUpForm__form">
         <div className="signUpForm__formField">
           <label htmlFor="sign-firstName">First Name</label>
-          <input required id="sign-firstName" type="text" />
+          <input name="firstName" onChange={ (e) => handleChange(e) } required id="sign-firstName" type="text" />
         </div>
         <div className="signUpForm__formField">
           <label htmlFor="sign-lastName">Last Name</label>
-          <input required id="sign-lastName" type="text" />
+          <input name="lastName" onChange={ (e) => handleChange(e) } required id="sign-lastName" type="text" />
         </div>
         <div className="signUpForm__formField">
           <label htmlFor="sign-email">Email</label>
-          <input required id="sign-email" type="email" />
+          <input name="email" onChange={ (e) => handleChange(e) } required id="sign-email" type="email" />
         </div>
         <div className="signUpForm__formField">
           <label htmlFor="sign-password">Password</label>
-          <input required id="sign-password" type="password" />
+          <input name="password" onChange={ (e) => handleChange(e) } required id="sign-password" type="password" />
         </div>
-        <button className="signUpForm__submit">Create Account</button>
+        <button type="submit" className="signUpForm__btn">Create Account</button>
+        <Link to="/login/" exact className="signUpForm__already">Already have an account? Login</Link>
       </form>
     </div>
   );
--- frontend/src/data/hero.js
+++ frontend/src/data/hero.js
@@ -1,12 +1,13 @@
 import investImage from "../svgs/invest.svg";
 import mobile from "../svgs/mobile.svg";
 import guaranteeImage from "../svgs/guarantee.svg";
+import { Link } from "react-router-dom";
 
 export const invest = {
   header: <h1 className="heroSection__header">Ejeka Invest</h1>,
   body:
     "A platform that empowers you like never before, to make investments and get returns from those investments",
-  button: <button className="heroSection__btn">Create an Account</button>,
+  button: <Link to="/signup/" exact className="heroSection__btn">Create an Account</Link>,
   image: investImage,
 };
 export const deposit = {
